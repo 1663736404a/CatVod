a3e2ed7cef57ab312ff4b05712837b8abe962b3f
1663736404a <164669635+1663736404a@users.noreply.github.com>
2026-08-21 12:37:10 +0800

    fix(youtube): use SABR segment manifest route

--- app/src/main/java/com/github/catvod/spider/YouTube.java
+++ app/src/main/java/com/github/catvod/spider/YouTube.java
@@ -183,7 +183,7 @@ public class YouTube extends Spider {
         int at = rawPid.lastIndexOf('@');
         if (at > 0) videoId = rawPid.substring(0, at);
         String quality = YouTubeLite.optString(ext, "quality", "best");
-        String url = Proxy.getUrl(siteKey, "&type=sabr_mpd2&vid=" + Uri.encode(videoId)
+        String url = Proxy.getUrl(siteKey, "&type=sabr_mpd&vid=" + Uri.encode(videoId)
                 + "&quality=" + Uri.encode(quality));
         return Result.get().url(url).dash().string();
     }
